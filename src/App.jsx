1125955c3c073db8b0096089b61e401c2215f5f0
Barry Carroll <itsmrbarry@gmail.com>
2025-08-19 21:51:30 +0100

    refactor: move list data into App component and update List to accept stories as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
 import './App.css'
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://react.dev/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const Search = () => {
   
   const handleChange = (event) => {
@@ -34,27 +15,58 @@ const Search = () => {
   )
 }
 
-const List = () => 
-  <ul>
-    {list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </li> 
-    ))}
-  </ul>
+const List = (props) =>  {
+  return (
+    <ul>
+      {props.stories.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  );
+}
 
+const Item = (props) => {
+  return (
+    <li>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li> 
+  )
+}
 
-const App = () => 
-  <div>
-      <h1>My Hacker Stories</h1>
-      <Search />
-      <hr />
-      <List />    
-  </div>
+const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://react.dev/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  return (
+    <div>
+        <h1>My Hacker Stories</h1>
+        <Search />
+        <hr />
+        <List stories={stories} />    
+    </div>
+  );
+}
 
 export default App
