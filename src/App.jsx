73b778d0d6fb9b4f651ce81d01fa2afe2b3afce1
Barry Carroll <itsmrbarry@gmail.com>
2025-08-21 22:53:48 +0100

    use nested destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -57,14 +57,22 @@ const List = ({ stories }) => (
   </ul>
 )
 
-const Item = ({ item }) => (
+const Item = ({ 
+  item  : {
+    url,
+    title,
+    author,
+    num_comments,
+    points,
+  }
+}) => (
   <li>
     <span>
-      <a href={item.url}>{item.title}</a>
+      <a href={url}>{title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
   </li>
 )
 
